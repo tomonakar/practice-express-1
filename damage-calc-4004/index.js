05628daacbcdc8179a3e0da6543734dcd9fed91e
tomonakar <tomohisa.nak@gmail.com>
2019-07-03 10:38:22 +0900

    fix: 2000より大きい異常値の分岐を追加

--- damage-calc-4004/index.js
+++ damage-calc-4004/index.js
@@ -1,23 +1,23 @@
-'use strict';
+"use strict"
 
 /**
  * ダメージが防御力や防御力貫通によってどのようなダメージになるのかを計算する関数
- * 
+ *
  * 負の入力値があった場合には0として扱い、2000以上の入力値は2000として扱う。
  * 実効防御力は、防御力 - 防御力貫通 で定義され、
  * この実行防御力は、0未満にはならない。
  * ダメージ減少率は、実効防御力 / (100 + 実効防御力) で定義され、
  * ダメージは、攻撃力 * (1 - ダメージ減少率) を小数点以下で四捨五入した値となる。
- * 
+ *
  * @param {Number} power 攻撃力
  * @param {Number} armor 防御力
  * @param {Number} armorPenetration 防御力貫通
  * @return {Number} ダメージ
  */
 function effectiveDamage(power, armor, armorPenetration) {
-  let effectiveArmor = normalize(armor) - normalize(armorPenetration);
-  const damageDecrease = effectiveArmor / (100 + effectiveArmor);
-  return Math.floor(normalize(power) * (1 - damageDecrease));
+  let effectiveArmor = normalize(armor) - normalize(armorPenetration)
+  const damageDecrease = effectiveArmor / (100 + effectiveArmor)
+  return Math.floor(normalize(power) * (1 - damageDecrease))
 }
 
 /**
@@ -27,12 +27,14 @@ function effectiveDamage(power, armor, armorPenetration) {
  */
 function normalize(n) {
   if (n < 0) {
-    return 0;
+    return 0
+  } else if (n >= 2000) {
+    return 2000
   } else {
-    return n;
+    return n
   }
 }
 
 module.exports = {
-  effectiveDamage: effectiveDamage
-};
+  effectiveDamage: effectiveDamage,
+}
