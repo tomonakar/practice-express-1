0a70eaf212b5b8b91716bcf1728d4c659daa51cc
tomonakar <tomohisa.nak@gmail.com>
2019-07-03 10:37:18 +0900

    test: 負の異常値, 2000より大きい異常値のテストを追加

--- damage-calc-4004/test/test.js
+++ damage-calc-4004/test/test.js
@@ -6,4 +6,16 @@ describe("#effectiveDamage()", () => {
   it("正常なダメージ計算ができる", () => {
     assert.equal(dc.effectiveDamage(100, 50, 30), 83)
   })
+
+  it("負の異常値におけるダメージ計算ができる", () => {
+    assert.equal(dc.effectiveDamage(-1, 0, 0), 0)
+    assert.equal(dc.effectiveDamage(0, -1, 0), 0)
+    assert.equal(dc.effectiveDamage(0, 0, -1), 0)
+  })
+
+  it("2000より大きい異常値におけるダメージ計算ができる", () => {
+    assert.equal(dc.effectiveDamage(2001, 0, 0), 2000)
+    assert.equal(dc.effectiveDamage(300, 2001, 0), 14)
+    assert.equal(dc.effectiveDamage(300, 2000, 2001), 300)
+  })
 })
