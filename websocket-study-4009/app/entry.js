7c2a68dfd26ee3e2b39a3fa2bd5ec6eb2e30a58f
tomonakar <tomohisa.nak@gmail.com>
2019-07-04 11:10:00 +0900

    feat: クライアントサイドの通信をwebsocketに変更

--- websocket-study-4009/app/entry.js
+++ websocket-study-4009/app/entry.js
@@ -1,24 +1,29 @@
-'use strict';
-import $ from 'jquery';
-const block = $('#block');
-const scalingButton = $('#scaling-button');
+"use strict"
+import $ from "jquery"
+import io from "socket.io-client"
+const block = $("#block")
+const scalingButton = $("#scaling-button")
 
 scalingButton.click(() => {
-  block.animate({ width: '200pt', height: '200pt' }, 2000);
-  block.animate({ width: '100pt', height: '100pt' }, 2000);
-});
+  block.animate({ width: "200pt", height: "200pt" }, 2000)
+  block.animate({ width: "100pt", height: "100pt" }, 2000)
+})
 
-const movingButton = $('#moving-button');
+const movingButton = $("#moving-button")
 
 movingButton.click(() => {
-  block.animate({ 'marginLeft': '500px' }, 500);
-  block.animate({ 'marginLeft': '20px' }, 1000);
-});
+  block.animate({ marginLeft: "500px" }, 500)
+  block.animate({ marginLeft: "20px" }, 1000)
+})
 
-const loadavg = $('#loadavg');
+const loadavg = $("#loadavg")
 
-setInterval(() => {
-  $.get('/server-status', {}, (data) => {
-    loadavg.text(data.loadavg.toString());
-  });
-}, 10);
+// websocketの接続先を定義
+const socket = io("http://localhost:8000")
+
+// websocket接続上で、 server-status イベントが発火したら
+// view を受け取ったデータで更新する
+// AJAXの時はクライアントサイドでポーリング間隔を指定してたが、websocketではもちろん不要
+socket.on("server-status", data => {
+  loadavg.text(data.loadavg.toString() + "です！！")
+})
