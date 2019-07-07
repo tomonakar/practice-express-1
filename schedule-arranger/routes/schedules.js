31f29870e95df2277e13fe14ac3c651ba62eb9fc
tomonakar <tomohisa.nak@gmail.com>
2019-07-07 18:49:01 +0900

    feat: 閲覧ユーザに紐付く出欠処理を追加

--- schedule-arranger/routes/schedules.js
+++ schedule-arranger/routes/schedules.js
@@ -76,13 +76,39 @@ router.get("/:scheduleId", authenticationEnsurer, (req, res, next) => {
             availabilityMapMap.set(a.user.userId, map)
           })
 
+          // 閲覧ユーザーと出欠に紐づくユーザーからユーザー Map (キー:ユーザー ID, 値:ユーザー) を作る
+          const userMap = new Map() // key: userId, value: User
+          userMap.set(parseInt(req.user.id), {
+            isSelf: true,
+            userId: parseInt(req.user.id),
+            username: req.user.username,
+          })
+          availabilities.forEach(a => {
+            userMap.set(a.user.userId, {
+              isSelf: parseInt(req.user.id) === a.user.userId, // 閲覧ユーザー自身であるかを含める
+              userId: a.user.userId,
+              username: a.user.username,
+            })
+          })
+
+          // 全ユーザー、全候補で二重ループしてそれぞれの出欠の値がない場合には、「欠席」を設定する
+          const users = Array.from(userMap).map(keyValue => keyValue[1])
+          users.forEach(u => {
+            candidates.forEach(c => {
+              const map = availabilityMapMap.get(u.userId) || new Map()
+              const a = map.get(c.candidateId) || 0 // デフォルト値は 0 を利用
+              map.set(c.candidateId, a)
+              availabilityMapMap.set(u.userId, map)
+            })
+          })
+
           console.log("availability", availabilityMapMap) // TODO 除去する
 
           res.render("schedule", {
             user: req.user,
             schedule: schedule,
             candidates: candidates,
-            users: [req.user],
+            users: users,
             availabilityMapMap: availabilityMapMap,
           })
         })
