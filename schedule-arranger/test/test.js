28ae414079664d5dade5829bf4afc12330c76d5e
tomonakar <tomohisa.nak@gmail.com>
2019-07-07 17:15:13 +0900

    test: 予定作成後の表示内容のテストを追加

--- schedule-arranger/test/test.js
+++ schedule-arranger/test/test.js
@@ -1,7 +1,10 @@
 "use strict"
-const request = require("supertest")
-const app = require("../app")
-const passportStub = require("passport-stub")
+let request = require("supertest")
+let app = require("../app")
+let passportStub = require("passport-stub")
+let User = require("../models/user")
+let Schedule = require("../models/schedule")
+let Candidate = require("../models/candidate")
 
 describe("/login", () => {
   before(() => {
@@ -36,5 +39,59 @@ describe("/logout", () => {
       .get("/logout")
       .expect("Location", "/")
       .expect(302, done)
+  })
+})
+
+describe("/schedules", () => {
+  before(() => {
+    passportStub.install(app)
+    passportStub.login({ id: 0, username: "testuser" })
+  })
+
+  after(() => {
+    passportStub.logout()
+    passportStub.uninstall(app)
+  })
+
+  it("予定が作成でき、表示される", done => {
+    User.upsert({ userId: 0, username: "testuser" }).then(() => {
+      request(app)
+        .post("/schedules")
+        .send({
+          scheduleName: "テスト予定1",
+          memo: "テストメモ1\r\nテストメモ2",
+          candidates: "テスト候補1\r\nテスト候補2\r\nテスト候補3",
+        })
+        .expect("Location", /schedules/)
+        .expect(302)
+        .end((err, res) => {
+          let createdSchedulePath = res.headers.location
+          request(app)
+            .get(createdSchedulePath)
+            .expect(/テスト予定1/)
+            .expect(/テストメモ1/)
+            .expect(/テストメモ2/)
+            .expect(/テスト候補1/)
+            .expect(/テスト候補2/)
+            .expect(/テスト候補3/)
+            .expect(200)
+            .end((err, res) => {
+              // テストで作成したデータを削除
+              let scheduleId = createdSchedulePath.split("/schedules/")[1]
+              Candidate.findAll({
+                where: { scheduleId: scheduleId },
+              }).then(candidates => {
+                candidates.forEach(c => {
+                  c.destroy()
+                })
+                Schedule.findById(scheduleId).then(s => {
+                  s.destroy()
+                })
+              })
+              if (err) return done(err)
+              done()
+            })
+        })
+    })
   })
 })
