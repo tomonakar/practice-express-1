e9e9a745bda20dc7383a0036a3308500619c5778
tomonakar <tomohisa.nak@gmail.com>
2019-07-07 15:29:46 +0900

    feat: sequelizeを使用してリレーションを設定

--- schedule-arranger/app.js
+++ schedule-arranger/app.js
@@ -7,6 +7,24 @@ var helmet = require("helmet")
 var session = require("express-session")
 var passport = require("passport")
 
+// モデルの読み込み
+var User = require("./models/user")
+var Schedule = require("./models/schedule")
+var Availability = require("./models/availability")
+var Candidate = require("./models/candidate")
+var Comment = require("./models/comment")
+User.sync().then(() => {
+  Schedule.belongsTo(User, { foreignKey: "createdBy" })
+  Schedule.sync()
+  Comment.belongsTo(User, { foreignKey: "userId" })
+  Comment.sync()
+  Availability.belongsTo(User, { foreignKey: "userId" })
+  Candidate.sync().then(() => {
+    Availability.belongsTo(Candidate, { foreignKey: "candidateId" })
+    Availability.sync()
+  })
+})
+
 var SECRET_KEYS = require("./secret-keys.js")
 var GitHubStrategy = require("passport-github2").Strategy
 var GITHUB_CLIENT_ID = SECRET_KEYS.GITHUB_CLIENT_ID
